60bca58fe36898500b0043bcd8980bc4a1768a77
flashtags922 <louillalim.85@gmail.com>
2026-05-14 12:38:08 +0800

    Update OrbitalTrident.java

--- src/main/java/me/anti/orbital/OrbitalTrident.java
+++ src/main/java/me/anti/orbital/OrbitalTrident.java
@@ -1,12 +1,17 @@
 package me.anti.orbital;
 
 import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.entity.Trident;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OrbitalTrident extends JavaPlugin implements Listener {
@@ -14,7 +19,30 @@ public class OrbitalTrident extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("OrbitalTrident enabled");
+    }
+
+    @Override
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
+
+        if (!(sender instanceof Player)) return true;
+
+        Player player = (Player) sender;
+
+        ItemStack trident = new ItemStack(Material.TRIDENT);
+
+        ItemMeta meta = trident.getItemMeta();
+        meta.setDisplayName("§cOrbital Trident");
+
+        trident.setItemMeta(meta);
+
+        player.getInventory().addItem(trident);
+
+        return true;
     }
 
     @EventHandler
@@ -35,17 +63,17 @@ public class OrbitalTrident extends JavaPlugin implements Listener {
         for (int i = 0; i < 40; i++) {
 
             double angle = Math.toRadians(i * 9);
-            double radius = 5;
 
-            double x = Math.cos(angle) * radius;
-            double z = Math.sin(angle) * radius;
+            double x = Math.cos(angle) * 5;
+            double z = Math.sin(angle) * 5;
 
             Location spawn = center.clone().add(x, 80, z);
 
-            TNTPrimed tnt = (TNTPrimed) world.spawnEntity(
-                    spawn,
-                    EntityType.TNT
-            );
+            TNTPrimed tnt =
+                    (TNTPrimed) world.spawnEntity(
+                            spawn,
+                            EntityType.TNT
+                    );
 
             tnt.setFuseTicks(60);
             tnt.setYield(8f);
